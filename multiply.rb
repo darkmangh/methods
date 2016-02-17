dc8d93c1174b8c3b57b5eb8b710b0590d989b875
darkmangh <charles_rex@yahoo.com>
2016-02-17 19:04:03 +0000

    multiply

---
+++ multiply.rb
@@ -0,0 +1,16 @@
+def multiply(a,b)
+  puts "Your results is:"
+  puts a * b
+
+
+end
+
+puts "Enter your first number:"
+
+a = gets.chomp.to_i
+
+puts "Enter your second number"
+
+b = gets.chomp.to_i
+
+multiply(a,b)
